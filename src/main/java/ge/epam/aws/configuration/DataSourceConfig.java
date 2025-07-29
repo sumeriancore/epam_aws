3778162f99aaed45ec7f722097b3bea69f43f08e
sumeriancore <stepan_stepan_1212@mail.ru>
2025-07-29 14:57:06 +0400

    working, but need some CF improvements

--- src/main/java/ge/epam/aws/configuration/DataSourceConfig.java
+++ src/main/java/ge/epam/aws/configuration/DataSourceConfig.java
@@ -2,9 +2,7 @@ package ge.epam.aws.configuration;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DataSourceConfig {
